38e489b304827ef48b2e07f7388f6193c37350a9
Avatar <codecj93@gmail.com>
2024-07-28 22:08:05 -0400

    Landing 'About Me' Page edited to include text

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import React from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+    <div>
+      <section>
+        <h1><b>Christopher Josephs</b></h1>
+        <div id="links">
+          <a href="#MyWork">My Work</a>
+          <a href="#ContactMe">Contact Me</a>
+          <a href="Resume">Resume</a>
+        </div>
+      </section>
+      <section id="AboutMe">
+        <h2><u>About Me</u></h2>
+        <p id="bio">
+          My name is Christopher Josephs, a junior developer originally from Brooklyn, NY but currently living in Harrisburg, PA. I have experience in full stack
+          development. I have experience in utilizing HTML, CSS, JavaScript, MongoDB, React, and PostgreSQL. I am interested in working with small businesses and freelance work.
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+        <p>Click on the links above for more information!</p>
+        <img id="self" src="./9E051D28-D067-46E4-A4C0-AED9EC2B8286.jpg" alt="Picture of Christopher Josephs" />
+      </section>
+    </div>
+  );
 }
 
-export default App
+
+
+export default App;
+
