53f6dba5ed3bfd9b20ac361121d81d3244f28da3
Avatar <codecj93@gmail.com>
2024-07-31 00:30:16 -0400

    l

--- src/pages/MyWork.jsx
+++ src/pages/MyWork.jsx
@@ -2,6 +2,7 @@ import react from 'react';
 import Footer from '../components/Footer';
 import '/Users/cjosephs93/Desktop/Bootcamp/challenges/React Portfolio/React-Portfolio/src/App.css';
 import ProjectCard from '../components/ProjectCard';
+import '/Users/cjosephs93/Desktop/Bootcamp/challenges/React Portfolio/React-Portfolio/src/App.css';
 
 
 
